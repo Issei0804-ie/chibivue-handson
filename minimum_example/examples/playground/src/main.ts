3476e82a7e2c67d062547eb72cf360d40489dacd
issei miyara <issei_miyara@rebuild-inc.co.jp>
2024-02-19 22:42:14 +0900

    小さい Reactivity System

--- minimum_example/examples/playground/src/main.ts
+++ minimum_example/examples/playground/src/main.ts
@@ -1,20 +1,26 @@
 import { createApp } from "chibivue";
 import {h} from "../../../packages/runtime-core/h";
+import {reactive} from "../../../packages/reactivity/reactive";
 
 const app = createApp({
-    render() {
-        return h('div', { id: 'my-app' }, [
-            h('p', { style: 'color: red; font-weight: bold;' }, ['Hello world.']),
-            h(
-                'button',
-                {
-                    onClick() {
-                        alert('Hello world!')
+    setup() {
+        const state = reactive({count: 0})
+        const increment = () => {
+            state.count++
+        }
+
+        return function render() {
+            return h('div', { id: 'my-app' }, [
+                h('p', {}, [`count: ${state.count}`]),
+                h(
+                    'button',
+                    {
+                        onClick: increment,
                     },
-                },
-                ['click me!'],
-            ),
-        ])
+                    ['click me!'],
+                ),
+            ])
+        }
     },
 })
 
